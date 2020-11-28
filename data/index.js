d2048f0143848cee80dde02877d4e1881f08989b
djviodes <djviodes26@gmail.com>
2020-11-27 18:57:15 -0800

    Fixed up the code. Made it look nice

--- data/index.js
+++ data/index.js
@@ -5,17 +5,17 @@ const getRecipes = () => {
 };
 
 const getShoppingList = recipe_id => {
-   return db('recipes_ingredients as r_i')
-   .where({recipe_id})
-   .join('ingredients as i', 'r_i.ingredient_id', '=', 'i.id')
-   .select('i.*');
+    return db('recipes_ingredients as r_i')
+        .where({recipe_id})
+        .join('ingredients as i', 'r_i.ingredient_id', '=', 'i.id')
+        .select('i.*');
 };
 
 const getInstructions = (recipe_id) => {
     return db('recipes_steps as r_s')
-    .where({recipe_id})
-    .join('steps as s', 'r_s.steps_id', '=', 's.id')
-    .select('s.*');
+        .where({recipe_id})
+        .join('steps as s', 'r_s.steps_id', '=', 's.id')
+        .select('s.*');
 };
 
 module.exports = {
