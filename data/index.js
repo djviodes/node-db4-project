bdfbdeb444306aa574e6ee4eb15bfafafd311c57
djviodes <djviodes26@gmail.com>
2020-11-27 18:55:45 -0800

    Finished MVP

---
+++ data/index.js
@@ -0,0 +1,25 @@
+const db = require('./db-config');
+
+const getRecipes = () => {
+    return db('recipes');
+};
+
+const getShoppingList = recipe_id => {
+   return db('recipes_ingredients as r_i')
+   .where({recipe_id})
+   .join('ingredients as i', 'r_i.ingredient_id', '=', 'i.id')
+   .select('i.*');
+};
+
+const getInstructions = (recipe_id) => {
+    return db('recipes_steps as r_s')
+    .where({recipe_id})
+    .join('steps as s', 'r_s.steps_id', '=', 's.id')
+    .select('s.*');
+};
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions
+}
